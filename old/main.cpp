6f7df87ad6964bc13b0fbdc4da7e4de3fb291c97
MatteoEnsea <matt.doffe@gmail.com>
2025-09-24 13:12:56 +0200

    Adding fsm

---
+++ old/main.cpp
@@ -0,0 +1,28 @@
+#include "pokemon.hpp"
+#include "csvReader.hpp"
+#include "pokedex.hpp"
+
+#include <iostream>
+
+int main2()
+{
+
+    Pokedex& dex = Pokedex::getInstance("pokedex.csv");
+    Pokemon p = dex.cloneByName("Charmander");
+    p.displayInfo();
+
+
+    Pokemon salameche(1,"Salameche",5,3,2,1);
+
+    Pokemon pickachu(2,"Pickachu",5,3,2,1);
+
+    salameche.displayInfo();
+
+    std::cout << "HP of salameche: " << salameche.getHitPoint() << std::endl;
+
+    for (int i = 0; i < 5; i++) {
+        salameche.attackAnotherPokemon(pickachu);
+    }
+
+    return 0;
+}
