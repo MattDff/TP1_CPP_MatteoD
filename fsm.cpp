12d0d903315a2168d4e97c6c2a3fdc1b79f56857
MatteoEnsea <matt.doffe@gmail.com>
2025-09-25 17:27:11 +0200

    add fsm

--- fsm.cpp
+++ fsm.cpp
@@ -1,15 +1,20 @@
 #include "fsm.h"
+#include "pokedex.hpp"
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
+// ========== AbstractState ==========
 AbstractState::AbstractState(FiniteStateMachine* stateMachine) : fsm(stateMachine) {}
 
+// ========== StateAccueil ==========
 StateAccueil::StateAccueil(FiniteStateMachine* stateMachine) : AbstractState(stateMachine) {}
 void StateAccueil::run() {
     std::cout << "Écran d'accueil - Appuyez sur une touche pour commencer...\n";
     fsm->setState(new StateExploration(fsm));
 }
 
+// ========== StateExploration ==========
 StateExploration::StateExploration(FiniteStateMachine* stateMachine) : AbstractState(stateMachine) {}
 void StateExploration::run() {
     std::cout << "Exploration... Rencontre aléatoire !\n";
@@ -21,37 +26,56 @@ void StateExploration::run() {
     }
 }
 
+// ========== StateCapturePokemon ==========
 StateCapturePokemon::StateCapturePokemon(FiniteStateMachine* stateMachine) : AbstractState(stateMachine) {}
 void StateCapturePokemon::run() {
-    std::cout << "Tentative de capture de Pokémon...\n";
+    Pokedex& pokedex = Pokedex::getInstance("pokemon.csv");
+    Pokemon wild = pokedex.cloneByName("Pikachu");
+    std::cout << "Un Pokémon sauvage apparait :\n";
+    wild.displayInfo();
+
     int succes = rand() % 2;
     if (succes) {
-        std::cout << "Capture réussie ! Retour à l'exploration.\n";
+        std::cout << "Capture réussie ! Vous avez attrapé " << wild.getName() << " !\n";
         fsm->setState(new StateExploration(fsm));
     } else {
-        std::cout << "Échec de capture. Retour à l'exploration.\n";
+        std::cout << "Le " << wild.getName() << " s'est échappé...\n";
         fsm->setState(new StateExploration(fsm));
     }
 }
 
+// ========== StateCombatArene ==========
 StateCombatArene::StateCombatArene(FiniteStateMachine* stateMachine) : AbstractState(stateMachine) {}
 void StateCombatArene::run() {
+    Pokedex& pokedex = Pokedex::getInstance("pokemon.csv");
+
+    Pokemon player = pokedex.cloneByName("Charmander");
+    std::cout << "Votre Pokémon :\n";
+    player.displayInfo();
+
+    Pokemon opponent = pokedex.cloneByName("Bulbasaur");
+    std::cout << "Adversaire :\n";
+    opponent.displayInfo();
+
     std::cout << "Combat dans l'arène...\n";
-    int victoire = rand() % 2;
-    if (victoire) {
-        std::cout << "Victoire ! Vous avez volé un Pokémon.\n";
+    player.attackAnotherPokemon(opponent);
+
+    if (opponent.getHitPoint() <= 0) {
+        std::cout << "Victoire ! Vous gagnez le combat et obtenez un Pokémon.\n";
         fsm->setState(new StateExploration(fsm));
     } else {
-        std::cout << "Défaite... GAME OVER.\n";
+        std::cout << "Votre Pokémon est K.O. GAME OVER.\n";
         fsm->setState(new StateGameOver(fsm));
     }
 }
 
+// ========== StateGameOver ==========
 StateGameOver::StateGameOver(FiniteStateMachine* stateMachine) : AbstractState(stateMachine) {}
 void StateGameOver::run() {
     std::cout << "GAME OVER.\n";
 }
 
+// ========== FSM ==========
 FiniteStateMachine::FiniteStateMachine() {
     srand(time(nullptr));
     currentState = new StateAccueil(this);
--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include "fsm.h"
-
+#include "pokedex.hpp"
+#include <iostream>
 
 int main() {
+
+    Pokedex& pokedex = Pokedex::getInstance("pokedex.csv");
+
+    // Créer la machine à états
     FiniteStateMachine fsm;
 
-    // Boucle du jeu
-    for (int i = 0; i < 10; i++) { // 10 itérations max pour l'exemple
+    std::cout << "===== Début du jeu Pokémon =====\n";
+
+    // Boucle de jeu (10 itérations pour tester rapidement)
+    for (int i = 0; i < 10; i++) {
+        std::cout << "\n--- Tour " << (i+1) << " ---\n";
         fsm.execute();
     }
 
+    std::cout << "\n===== Fin de la partie =====\n";
     return 0;
 }
